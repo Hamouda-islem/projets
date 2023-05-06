37cfce04d35c779ad4d7b81588e522e97beedb0d
Hamouda-islem <129670212+Hamouda-islem@users.noreply.github.com>
2023-05-06 17:36:24 +0200

    Update faitdon.php

--- faitdon.php
+++ faitdon.php
@@ -1,3 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ma page de rendez-vous</title>
+<style>
+:root {
+  --bg-color: #1f242d;
+  --second-bg-color: #323946;
+  --text-color: #fff;
+  --main-color: #0ef;
+}
+
+body {
+  background-color: var(--bg-color);
+  color: var(--text-color);
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+color: var(--main-color);
+text-align: center;
+margin-top: 50px;
+}
+
+.container {
+  background-color: var(--second-bg-color);
+  border-radius: 10px;
+  padding: 20px;
+  margin: 20px auto;
+  max-width: 500px;
+}
+
+.btn {
+  background-color: var(--main-color);
+  color: var(--text-color);
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.btn:hover {
+  background-color: #0bd;
+}
+</style>
+</head>
+<body>
+<h1><span id="typed-title"></span></h1>
+
+<div class="container">
 <?php
 $host = 'localhost';
 $dbname = 'projetsang';
@@ -36,10 +87,22 @@ if(isset($_POST['Faire']) && isset($_POST['id'])) {
             'centre' => $centre,
         ]);
         if ($result) {
-            echo "<script>alert('RDV de don ajouté avec succès')window.location.replace('.thtml')</script>";
+            echo "<h2>RDV de don ajouté avec succès</h2>";
         } else {
             echo "Erreur lors de l'ajout du don";
         }
     }
 }
 ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+<script>
+var typed = new Typed('#typed-title', {
+strings: ['RDV'],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 0,
+loop: true
+});
+</script>
+</body>
+</html>
