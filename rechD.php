f12fee4c3012d1cb2ea2d051da323b00a185632c
Hamouda-islem <129670212+Hamouda-islem@users.noreply.github.com>
2023-05-06 18:00:45 +0200

    Update rechD.php

--- rechD.php
+++ rechD.php
@@ -3,64 +3,120 @@
 <head>
 	<title>Ma page de rendez-vous</title>
 	<style>
-        :root {
-    --bg-color: #1f242d;
-    --second-bg-color: #323946;
-    --text-color: #fff;
-    --main-color: #0ef;
+     
+
+     :root {
+  --bg-color: #1f242d;
+  --second-bg-color: #323946;
+  --text-color: #fff;
+  --main-color: #0ef;
+  --danger-color: #f44336; /* Couleur rouge pour les boutons "Annuler" */
 }
 
 body {
-    background-color: var(--bg-color);
-    font-family: Arial, sans-serif;
+  background-color: var(--bg-color);
+  color: var(--text-color);
+  font-family: sans-serif;
 }
 
-h2 {
-    color: var(--text-color);
+h1 {
+  color: #0ef;
+  text-align: center;
+  /* Ajout de l'attribut id pour le titre h1 */
+  /* Ajout de la classe typed pour utiliser la bibliothèque Typed.js */
+  /* Ajout de la classe cursor pour afficher le curseur de la bibliothèque Typed.js */
+  /* Ajout de la propriété font-size pour augmenter la taille du titre */
+  font-size: 36px;
+}
+/* Ajout de la classe icon pour les icônes */
+.icon {
+  display: none;
+  width: 0px;
+  height: 0px;
+  margin-right: 5px;
+  background-repeat: no-repeat;
+  background-position :center;
+  background-size: contain;
+}
+/* Ajout de la classe danger pour les boutons "Annuler" */
+.danger {
+  background-color: var(--danger-color);
+  color: var(--text-color);
 }
 
 table {
-    border-collapse: collapse;
-    width: 100%;
-    margin: 20px 0;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  border: 1px solid black;
+  padding: 12px;
+  text-align: center;
 }
 
 th {
-    background-color: var(--second-bg-color);
-    color: var(--text-color);
-    border: 1px solid black;
-    padding: 10px;
-    font-weight: bold;
-    text-align: left;
+  border-bottom: 1px solid;
 }
 
-td {
-    border: 1px solid black;
-    padding: 10px;
-}
-
-tr:nth-child(even) {
-    background-color: var(--second-bg-color);
+tbody tr:nth-child(even) {
+  background-color: var(--second-bg-color);
 }
 
 button {
-    background-color: var(--main-color);
-    color: var(--text-color);
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    font-weight: bold;
-    cursor: pointer;
+  background-color: var(--main-color);
+  border: none;
+  border-radius: 4px;
+  color: var(--text-color);
+  cursor: pointer;
+  font-size: 16px;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.3s ease;
 }
 
-input[type=text], select {
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    width: 100%;
+
+button:hover {
+  background-color:none;
 }
 
+.action-buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+/* Ajout de la classe icon pour les icônes */
+.icon {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+}
+/* Ajout de la classe danger pour les boutons "Annuler" */
+.danger {
+  background-color: var(--danger-color);
+  color: var(--text-color);
+}
+
+
+.action-buttons button {
+  margin-top: 10px;
+}
+*{
+      font-family: 'Roboto', sans-serif;
+    }
+.notification {
+  background-color: #f44336;
+  color: white;
+  text-align: center;
+  padding: 10px;
+  margin-bottom: 10px;
+}
     </style>
 </head>
 <body>
@@ -99,7 +155,7 @@ if(isset($_POST['rechercherD'])) {
         echo "Aucun donneur trouvé";
     } else {
         // Afficher les résultats sous forme de tableau
-        echo "<h2>Résultats de la Recherche</h2>";
+        echo "<h1>Résultats de la Recherche</h1>";
         echo '<table style="border-collapse: collapse; width: 100%;">';
         echo '<thead>';
         echo '<tr>';
